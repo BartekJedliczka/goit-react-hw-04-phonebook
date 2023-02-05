2b4f992fc5df18fec0c9c80219e7743828225608
BartekJedliczka <bartekjedliczka@gmail.com>
2023-02-05 12:12:40 -0500

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,28 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import css from './Filter.module.css';
 import PropTypes from 'prop-types';
 
-class Filter extends Component {
-  render() {
-    const { value, onChange, result } = this.props;
-
-    return (
-      <div className={css.filter}>
-        <label className={css.label}>
-          Find contacts by Name
-          <input
-            className={css.input}
-            type="search"
-            value={value}
-            onChange={onChange}
-            placeholder="Enter name"
-          />
-        </label>
-        <p>Number of contacts: {result}</p>
-      </div>
-    );
-  }
-}
+const Filter = ({ value, onChange, result }) => {
+  return (
+    <div className={css.filter}>
+      <label className={css.label}>
+        Find contacts by Name
+        <input
+          className={css.input}
+          type="search"
+          value={value}
+          onChange={onChange}
+          placeholder="Enter name"
+        />
+      </label>
+      <p>Number of contacts: {result}</p>
+    </div>
+  );
+};
 
 Filter.propTypes = {
   value: PropTypes.string.isRequired,
